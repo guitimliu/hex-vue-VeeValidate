325e358132e15bd4add0010d580280f49987fbf9
Gui <guitimliu@gmail.com>
2022-03-11 01:09:18 +0800

    載入 Validate 套件、驗證資源、多國語言 CDN 與繁體中文語系，完成表單驗證

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue.esm-browser.min.js';
-
 const app = {
     data() {
         return {
@@ -12,6 +10,24 @@ const app = {
             },
         }
     },
+    components: {
+        VForm: VeeValidate.Form,
+        VField: VeeValidate.Field,
+        ErrorMessage: VeeValidate.ErrorMessage,
+    },
 }
 
-createApp(app).mount('#app');
+// 載入驗證資源
+VeeValidate.defineRule('email', VeeValidateRules['email']);
+VeeValidate.defineRule('min', VeeValidateRules['min']);
+VeeValidate.defineRule('required', VeeValidateRules['required']);
+
+// 載入台灣語系
+VeeValidateI18n.loadLocaleFromURL('./zh_TW.json');
+
+VeeValidate.configure({
+  generateMessage: VeeValidateI18n.localize('zh_TW'),
+  validateOnInput: false, // 是否於輸入文字時執行，或是移開後執行
+});
+
+Vue.createApp(app).mount('#app');
